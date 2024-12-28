6f156fa36e6637e5410a958bde3c2f8a4bc12b02
Mark Wilkinson <56736714+mewtonium@users.noreply.github.com>
2024-12-28 10:14:54 +0000

    Added priority icons to task lists

--- resources/js/Components/Tasks/TaskList.vue
+++ resources/js/Components/Tasks/TaskList.vue
@@ -2,7 +2,9 @@
 import EditTask from '@/Components/Tasks/EditTask.vue';
 import DeleteTask from '@/Components/Tasks/DeleteTask.vue';
 import CompleteTask from '@/Components/Tasks/CompleteTask.vue';
+import TaskPriorityIcon from '@/Components/Tasks/TaskPriorityIcon.vue';
 import { formatDate } from '@/helpers';
+import { usePage } from '@inertiajs/vue3';
 
 defineProps({
     tasks: {
@@ -14,6 +16,17 @@ defineProps({
         required: true
     },
 });
+
+const priorityNameByValue = (value) => {
+    const priorities = usePage().props.app.tasks.priorities;
+    const priority = Object.keys(priorities).find(key => priorities[key] === value);
+
+    if (priority === undefined) {
+        return ''
+    }
+
+    return priority;
+}
 </script>
 
 <template>
@@ -28,11 +41,14 @@ defineProps({
             </p>
 
             <template v-else>
-                <div class="divide-y">
+                <div class="divide-y dark:divide-gray-500">
                     <div v-for="task in tasks" :key="task.id" class="task py-2 flex justify-between">
                         <div class="text-left text-gray-900 dark:text-gray-100 flex items-center w-2/3">
                             <CompleteTask :task="task" class="task__complete mr-3" :dusk="`complete-task--${task.id}`" />
-                            <span class="task__title" :class="{ 'line-through text-gray-300 dark:text-gray-700': task.completed_at !== null }" v-text="task.title" />
+                            <div class="flex items-center space-x-1">
+                                <TaskPriorityIcon :priority="priorityNameByValue(task.priority)" />
+                                <span class="task__title truncate" :class="{ 'line-through text-gray-300 dark:text-gray-700': task.completed_at !== null }" v-text="task.title" />
+                            </div>
                         </div>
 
                         <div class="w-1/6 flex items-center justify-center">
---
+++ resources/js/Components/Tasks/TaskPriorityIcon.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ChevronDownIcon, ChevronUpIcon, ChevronDoubleUpIcon } from '@heroicons/vue/24/outline';
+
+defineProps({
+    priority: {
+        type: String,
+        required: true,
+    }
+});
+</script>
+
+<template>
+    <div class="size-5">
+        <ChevronDoubleUpIcon v-if="priority.toLowerCase() === 'urgent'" class="text-red-500" />
+        <ChevronUpIcon v-if="priority.toLowerCase() === 'high'" class="text-amber-500" />
+        <ChevronDownIcon v-if="priority.toLowerCase() === 'low'" class="text-blue-500" />
+    </div>
+</template>
